317febf1b75f47549a2dee6acb2821002d621cda
Tommy Werner <wtwerner@gmail.com>
2020-05-23 02:16:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,11 +18,11 @@ class Song
   end
   
   def self.genres
-    @@genres.
+    @@genres.uniq
   end
   
   def self.artists
-    @@artists.unique
+    @@artists.uniq
   end
   
   def self.genre_count
