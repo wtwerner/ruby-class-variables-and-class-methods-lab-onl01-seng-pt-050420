96028105fcd3d03db7d8819ecac0ddd27126178d
Tommy Werner <wtwerner@gmail.com>
2020-05-23 02:07:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,23 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize(name,artist,genre)
     @name=name
     @artist=artist
     @genre=genre
     @@count+=1
+    if @@genres.include?(genre)
+    else
+      @@genres << genre
+    end
   end
   
   def self.count
     @@count
   end
+  
+  def self.genres
+    
 end
